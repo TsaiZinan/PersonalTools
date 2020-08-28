5c07d492cca59f5fce828b498a84905a88dcdbbc
Tsai <czn826@gmail.com>
2020-08-28 17:25:03 +1000

    CopyPageCreate
    A page for copy different sentences in one click.

---
+++ vue_copy_ui/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+
+    <div v-for="(tax, index) in taxes" :key="index">
+      <copy />
+    </div>
+
+    <p>
+
+    <div class="add">
+      <vs-button
+        shadow
+        :active="active == 1"
+        @click="add"
+      >
+      Add New Line
+      </vs-button>
+    </div>
+    
+    <!-- <p>Count: {{taxes.length}}</p> -->
+
+    </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import Copy from './components/Copy.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      taxes: [0],
+      active: 1
+    }
+  },
+  methods:{
+    add: function(){
+      this.taxes.push(0);
+    }
+  },
+  components: {
+    //HelloWorld,
+    Copy
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.add {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  /* height: 100vh; */
+}
+</style>
---
+++ vue_copy_ui/src/components/Copy.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="center content-inputs">
+    <vs-input v-model="input_value" placeholder="Type something..."/>
+		<vs-button
+      shadow
+      :active="active == 1"
+      @click="doCopy();"
+    >
+		Copy
+    </vs-button>
+		<p></p>
+	</div>
+</template>
+
+<script>
+  export default {
+
+		data(){
+			return {
+				input_value: '',
+				active: 0
+			}
+		},
+		
+		methods:{
+			animation: function () {
+				this.active = 1;
+				this.active = 0;
+			},
+			doCopy: function () {
+				navigator.clipboard.writeText(this.input_value);
+			},
+			
+		}
+  }
+</script>
+
+<style>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  /* height: 100vh; */
+}
+</style>
